9fa698f55053dd18edd8251dff40becc246ceac9
Luke <lukem95@gmail.com>
2015-06-01 14:31:44 +0100

    Updated, again, to format used by lamassu-admin

--- test/ticker-test.js
+++ test/ticker-test.js
@@ -15,18 +15,21 @@
 'use strict';
 
 var test = require('tap').test;
-var ticker = require('../lib/index').ticker;
+var Ticker = require('../lib/ticker').factory();
 
-test('Read ticker from StartWallet', function(t){
-  var startwallet = new ticker();
-  startwallet.ticker(function(err, result) {
-    t.plan(5);
+test('Read ticker', function(t){
+  Ticker.ticker(['USD', 'EUR', 'ILS'], function(err, results) {
+    t.plan(5 * 3 + 2);
     t.equal(err, null, 'There should be no error');
-    t.notEqual(result, null, 'There should be a result');
-    t.equal(result.error, undefined, 'The result should contain no error');
-    t.notEqual(result["USD"], undefined, 'A rate should have been returned');
-    t.ok(parseFloat(result["USD"], 10), 'The rate should be a float');
+    t.equal(results.USD.currency, 'USD', 'Should have USD');
+    for (var currency in results) {
+      var result = results[currency];
+      t.notEqual(result, null, 'There should be a result');
+      t.equal(result.error, undefined, 'The result should contain no error');
+      t.notEqual(result.rate, undefined, 'A rate should have been returned');
+      t.equal(result.currency, currency, 'Currency should be ' + currency);
+      t.ok(parseFloat(result.rate, 10), 'The rate should be a float');
+    }
     t.end();
   });
 });
-
